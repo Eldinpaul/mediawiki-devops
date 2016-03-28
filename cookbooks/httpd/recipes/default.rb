71f740aa7a8b7c0cd34431899b4507a6de5054fc
ELDIN <eldhosepaulplachery@gmail.com>
2016-03-28 12:36:49 +0100

    cookbook updated

--- cookbooks/httpd/recipes/default.rb
+++ cookbooks/httpd/recipes/default.rb
@@ -1,5 +1,10 @@
-package "httpd" do
-  action :install
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
 end
 
 service "httpd" do
